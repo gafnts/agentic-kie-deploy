054af46e4f695a181b2a8180b04c303ca423e22e
Gabriel Fuentes <gafnts@gmail.com>
2026-05-01 17:28:52 -0600

    Bootstrap IAM module and add default tags to AWS provider

--- infra/main.tf
+++ infra/main.tf
@@ -1,5 +1,12 @@
 provider "aws" {
   region = var.aws_region
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "terraform"
+    }
+  }
 }
 
 resource "random_id" "bucket_suffix" {
