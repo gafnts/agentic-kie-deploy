c0d08ad8d9418369301491b8bf97c9447451e132
Gabriel Fuentes <gafnts@gmail.com>
2026-05-31 12:58:19 -0600

    Fix Athena results bucket name exceeding S3 63-character limit

--- infra/modules/results/locals.tf
+++ infra/modules/results/locals.tf
@@ -6,5 +6,5 @@ locals {
   # backtick-quoting; the workgroup follows the pipeline-wide hyphen naming.
   glue_database_name    = "${var.project_name}_${var.environment}_results"
   athena_workgroup_name = "${var.project_name}-${var.environment}-results"
-  athena_results_bucket = "${var.bucket_name}-athena-results"
+  athena_results_bucket = "${substr(var.bucket_name, 0, min(length(var.bucket_name), 48))}-athena-results"
 }
